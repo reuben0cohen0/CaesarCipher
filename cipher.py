14be63503701714e079f11e841c0b4f166ed6a87
r.d.cohen <r.d.cohen@student.saintkentigern.com>
2020-06-16 11:14:57 +1200

    v1 with comments

---
+++ cipher.py
@@ -0,0 +1,4 @@
+# rotational cipher
+
+
+# cipher key/decryption options
---
+++ gui.py
@@ -0,0 +1,13 @@
+# display window with cipher program on it
+
+
+# prompt user for input of text
+
+
+# convert text into cipher and display cipher key
+
+
+# cipher key/decryption options
+
+
+# decrypt button to decrypt messages
