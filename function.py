d16731ef54c99ef9e5402f5661bb7e7cc40f7086
Reuben Cohen <r.d.cohen@student.saintkentigern.com>
2020-08-14 15:59:28 +1200

    v17 - Potentially final

--- function.py
+++
@@ -1,9 +0,0 @@
-def rotate(letter, key):
-  if letter.islower():
-    new = ord(letter) + key
-    if new > ord('z'):
-      new = new - 26
-    new = chr(new)
-  else:
-    new = letter
-  return new
